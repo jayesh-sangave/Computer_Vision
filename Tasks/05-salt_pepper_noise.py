57baaa32bf2d110637c1e376f7dc9e1fd63a9f58
Jxy <jayesh.sangave.mtt@gmail.com>
2025-02-05 19:53:48 +0530

    very much difficulty

---
+++ Tasks/05-salt_pepper_noise.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("img/baboon.png")
+pixel_count = img.size
+
+def add_noise():
+    output = np.copy(img)
+
+    
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
