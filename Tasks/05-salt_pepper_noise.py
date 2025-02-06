efc16c9bd686ca70dcf8f5f7cc1565f37a367b4f
Jxy <jayesh.sangave.mtt@gmail.com>
2025-02-07 00:05:34 +0530

    modular Salt and Pepper

--- Tasks/05-salt_pepper_noise.py
+++ Tasks/05-salt_pepper_noise.py
@@ -2,20 +2,23 @@ import cv2
 import numpy as np
 
 img = cv2.imread("img/peppers.png")
-pixel_count = img.size
 
-output = np.copy(img)
-def add_noise():
+def add_noise(src):
+    pixel_count = src.size
+    output = np.copy(src)
+
     salt_count = int(pixel_count * 0.1)
     pepper_count = int(pixel_count * 0.1)
 
-    salt_pos = [np.random.randint(0, i-1, salt_count) for i in img.shape]
+    salt_pos = [np.random.randint(0, i-1, salt_count) for i in src.shape]
     output[salt_pos[0], salt_pos[1]] = 255
 
-    pepper_pos = [np.random.randint(0, i-1, pepper_count) for i in img.shape]
+    pepper_pos = [np.random.randint(0, i-1, pepper_count) for i in src.shape]
     output[pepper_pos[0], pepper_pos[1]] = 0
 
-add_noise()
+    return output
+
+img_noise = add_noise(img)
 
 cv2.imshow("original", img)
 cv2.imshow("output", output)
