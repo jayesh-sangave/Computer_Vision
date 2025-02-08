ef1d21563f619cb51ae07cd7ecde2717042a855b
Jxy <jayesh.sangave.mtt@gmail.com>
2025-02-08 15:25:55 +0530

    img resize

--- Tasks/07-morphological_operations.py
+++ Tasks/07-morphological_operations.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("img/MTT.png", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("img/MTT.png")
+img = cv2.resize(img, (300,300))
 
-kernel = np.ones((10,10), np.uint8)
-erosion = cv2.erode(img.copy(), kernel, 10)
-dilation = cv2.dilate(img.copy(), kernel, 10)
+kernel = np.ones((3,3), np.uint8)
+erosion = cv2.erode(img.copy(), kernel, 3)
+dilation = cv2.dilate(img.copy(), kernel, 3)
 
 row = np.hstack((img,erosion, dilation))
 
