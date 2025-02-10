b58880f9c96a9f79363182f98d02282313bb899e
Jxy <jayesh.sangave.mtt@gmail.com>
2025-02-10 19:07:50 +0530

    add trackbar

--- Tasks/08-trackbar.py
+++ Tasks/08-trackbar.py
@@ -1,22 +1,25 @@
 import cv2
 import numpy as np
 
+max_count = 100
+
 img = cv2.imread("img/peppers.png")
-
 cv2.imshow("original", img)
+cv2.namedWindow("Slider", cv2.WINDOW_AUTOSIZE)
 
-# def change_count():
-#     prob = cv2.getTrackbarPos("Salt & Pepper", "original")
+noisy_img = img.copy()
 
-#     return prob
+def noise(*args):
+    global noisy_img
+    prob = args[0]/100
+    noisy_img = add_noise(img, prob)
 
-def add_noise(src):
-    prob = cv2.getTrackbarPos("Salt & Pepper", "original")
+def add_noise(src, c):
     pixel_count = src.size
     output = np.copy(src)
 
-    salt_count = int(pixel_count * prob)
-    pepper_count = int(pixel_count * prob)
+    salt_count = int(pixel_count * c)
+    pepper_count = int(pixel_count * c)
 
     salt_pos = [np.random.randint(0, i-1, salt_count) for i in src.shape]
     output[salt_pos[0], salt_pos[1]] = 255
@@ -26,10 +29,16 @@ def add_noise(src):
 
     return output
 
-cv2.createTrackbar("Salt & Pepper", "original",0, 10, add_noise)
+
+cv2.createTrackbar("Percentage", "Slider", 1, 100, noise)
+
 
 while True:
-    img_noise = add_noise(img)
-    cv2.imshow("output", img_noise)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # noisy_img = add_noise(img)
+    cv2.imshow("output", noisy_img)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:  # If the ESC key is pressed, break the loop
+        break
+    # cv2.waitKey(0)
+
+cv2.destroyAllWindows()
